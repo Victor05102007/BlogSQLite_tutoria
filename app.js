06f27af69980f3f73fef1ab2c9cc4a71d5d458f6
Victor05102007 <victorciceri5@gmail.com>
2025-03-14 15:39:48 -0300

    Criação do banco de dados

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-const express = require("express");
+const express = require("express"); //importa lib express
+const sqlite3 = require("sqlite3"); //importa lib sqlite3
 
 const PORT = 8000; //porta tcp do servidor
 
 const app = express();
 
+const db = new sqlite3.Database("usar.db"); //uso do banco de dados
+db.serialize(() => {
+  //este método permite enviar comandos SQL em modo 'sequencial'
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
+  );
+});
+
+const index =
+  "<a href='/'>Home</a> <a href='/sobre'>Sobre</a> <a href='/cadastro'>Cadastro</a> <a href='/login'>Login</a>";
+const sobre = 'Vc está na página "sobre"<br><a href="/">Voltar</a>';
+const login = 'Vc está na página "login"<br><a href="/">voltar</a>';
+const cadastro = 'Vc está na página "cadastro"<br><a href="/">voltar</a>';
 //metodo express.get necessita de dois parâmetros
 //Na ARROW FUNCTION, O primeiro são os dados do servidor (REQUISITION)
 // O SEGUNDO, SÃO OS DADOS PROCESSADOS ENVIADOS AO CLIENT (RESULT - 'res)
 app.get("/", (req, res) => {
-  res.send("olá sesi!");
+  res.send(index);
+});
+
+app.get("/home", (req, res) => {
+  res.send(home);
 });
 
 app.get("/sobre", (req, res) => {
-  res.send("vc esta na pagina sobre");
+  res.send(sobre);
 });
 
-app.get("/endereco", (req, res) => {
-  res.send("vc esta na pagina endereço");
+app.get("/cadastro", (req, res) => {
+  res.send(cadastro);
 });
 
-app.get("/info", (req, res) => {
-  res.send("venha cá meu peixe q eu vou lhe ensinar");
+app.get("/login", (req, res) => {
+  res.send(login);
 });
 
 //app.çisten() deve ser o ultimo comando da aplicação
